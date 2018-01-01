e07d5aef4549b2b6ac8a64ab2ca425ba64cf4294
Konstantin Yakushin <y231966mdk@gmail.com>
2018-01-01 19:03:34 +0700

    Update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.send('./public/index.html');
